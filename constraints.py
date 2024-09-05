2148b395daa3f8c87544208b5248f2a3318c3056
Caldwell10 <caldwell.kibe@strathmore.edu>
2024-09-05 13:28:27 +0300

    Handled special characters

--- constraints.py
+++ constraints.py
@@ -1,3 +1,15 @@
+
+import re
+from functions import name
 """
 here we will handle all the constraints
 """
+
+def handle_special_characters():
+    """Checks if the name has special characters
+        Returns true if special characters are found, false otherwise"""
+
+    if re.search(r"[^-zA-Z\s,']",name):
+        return True
+    else:
+        return False
