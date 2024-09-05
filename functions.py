b80384b9eceef9f4017158812de9445afc0575d9
Caldwell10 <caldwell.kibe@strathmore.edu>
2024-09-05 12:39:37 +0300

    Added functions

--- functions.py
+++ functions.py
@@ -1,3 +1,23 @@
 """
 here we will define all the functions and handle each process
 """
+
+import  re
+
+
+def generate_email (name):
+    # Split the name (format: "Last Name, First Name Middle Names")
+    last_name,first_name = name.split(",")
+    #first name for word from the first name and strip any extra spaces
+    first_name=first_name.split()[0].strip
+    last_name=last_name.strip()
+    email=f"{first_name[0].lower()}{last_name.lower()}.@gmail.com"
+
+    return email
+
+if __name__ == "__main__":
+    name = input("Enter your name in 'Last Name ,First Name ,Middle Name(s) 'format:")
+    email=generate_email(name)
+    print(f"Your email is {email}")
+
+
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-name = "Caldwell"
-def sayhello(name):
-    print(f"Hello {name}")
+import pandas as pd
 
-sayhello(name)
+#load the Excel file
+file_a_df =pd.read_excel('/Users/caldwellwachira/PycharmProjects/codeLabs_1/ComputerGraphics/data/TestFiles.xlsx',sheet_name='File_A')
+file_b_df =pd.read_excel('/Users/caldwellwachira/PycharmProjects/codeLabs_1/ComputerGraphics/data/TestFiles.xlsx',sheet_name='File_B')
+
+# generate email
+def process_file(df):
+     return  print(df[['Student Name','Email Address']])
+
+print("Processing file_A")
+process_file(file_a_df)
+
+print("Processing file_B")
+process_file(file_b_df)
+
+
